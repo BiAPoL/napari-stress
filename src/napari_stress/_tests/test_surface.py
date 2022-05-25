a22fc85b4fd2bcb40d1900a1ae21e4dfac7e429d
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-05-25 18:18:06 +0200

    Add test

--- src/napari_stress/_tests/test_surface.py
+++ src/napari_stress/_tests/test_surface.py
@@ -5,7 +5,14 @@ def test_spherical_harmonics():
     import napari_stress
     import vedo
 
-    ellipse = vedo.shapes.Ellipsoid()
-    points = napari_stress.fit_spherical_harmonics(ellipse.points(), max_degree=3)
+    ellipse = vedo.shapes.Ellipsoid(res=100)
 
+    # Use default options (same number of points)
+    points = napari_stress.fit_spherical_harmonics(ellipse.points() * 100, max_degree=3)
     assert np.array_equal(ellipse.points().shape, points[:, 1:].shape)
+
+    # Use different number of points
+    points = napari_stress.fit_spherical_harmonics(ellipse.points() * 100, max_degree=2,
+                                                   n_points=1000)
+
+    assert len(points) == 1000
