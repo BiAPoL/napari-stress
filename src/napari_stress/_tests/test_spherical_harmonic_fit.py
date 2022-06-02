d21092116541026c301429a2388f35dbb86ac7d0
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-06-02 16:16:33 +0200

    added test for stress-spherical harmonics

--- src/napari_stress/_tests/test_spherical_harmonic_fit.py
+++ src/napari_stress/_tests/test_spherical_harmonic_fit.py
@@ -20,3 +20,16 @@ def test_spherical_harmonics():
     # Test default implementations
     points = napari_stress.fit_spherical_harmonics(ellipse.points(), max_degree=3)
     assert np.array_equal(ellipse.points().shape, points[:, 1:].shape)
+
+def test_stress_spherical_harmonics():
+
+    from napari_stress._spherical_harmonics._expansion import stress_spherical_harmonics_expansion
+    ellipse = vedo.shapes.Ellipsoid()
+
+    points, coefficients = stress_spherical_harmonics_expansion(ellipse.points(),
+                                                                max_degree=5)
+
+    assert np.array_equal(coefficients.shape, np.array([3, 6, 6]))
+
+if __name__ == '__main__':
+    test_stress_spherical_harmonics()
