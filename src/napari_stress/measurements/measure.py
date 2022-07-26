eeb455e4eccd0607d34c216240ea17e8b7abbe91
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2022-07-26 14:30:44 +0200

    modified decorator to replace annotations of abstract napari-stress types if we come from napari

--- src/napari_stress/measurements/measure.py
+++ src/napari_stress/measurements/measure.py
@@ -2,7 +2,20 @@
 from functools import wraps
 import inspect
 import napari
-import pandas as pd
+
+def convert(value, new_annotation):
+    """Check if an object is of a specific type and convert it if it isn't."""
+    from .._stress.manifold_SPB import manifold
+    from napari_stress import _METADATAKEY_MANIFOLD
+
+    if isinstance(value, new_annotation):
+        return value
+
+    if isinstance(value, napari.layers.Layer) and new_annotation is manifold:
+        return value.metadata[_METADATAKEY_MANIFOLD]
+    raise ValueError(f'Unsupported conversion {type(value)} -> {new_annotation}')
+
+
 
 def naparify_measurement(function):
 
@@ -10,33 +23,44 @@ def naparify_measurement(function):
     def wrapper(*args, **kwargs):
 
         sig = inspect.signature(function)
-        annotations = [
-            sig.parameters[key].annotation for key in sig.parameters.keys()
-            ]
+        # create mapping from position and keyword arguments to parameters
+        # will raise a TypeError if the provided arguments do not match the signature
+        # https://docs.python.org/3/library/inspect.html#inspect.Signature.bind
+        bound = sig.bind(*args, **kwargs)
+        # set default values for missing arguments
+        # https://docs.python.org/3/library/inspect.html#inspect.BoundArguments.apply_defaults
+        bound.apply_defaults()
 
-        # If a layer was passed instead of the necessary parameters:
-        # if napari.layers.Layer in annotations and :
-        if 'viewer' in kwargs:
-            new_args = []
-            metadata = args[0].metadata
-            for annotation in annotations:
-                for key in metadata.keys():
-                    if isinstance(metadata[key], annotation):
-                        new_args.append(metadata[key])
+        wrapper_sig = inspect.signature(wrapper)
 
-            features, metadata = function(*new_args, **kwargs)
+        for key in sig.parameters.keys():
+            value = bound.arguments[key]
+            original_annotation = sig.parameters[key].annotation
+            new_annotation = wrapper_sig.parameters[key].annotation
 
-            for key in features.keys():
-                args[0].features[key] = features[key]
+            print('Old:', original_annotation, 'New:', new_annotation)
+            if original_annotation is not new_annotation:
+                value = convert(value, original_annotation)
+                bound.arguments[key] = value
 
-            for key in metadata.keys():
-                args[0].metadata[key] = metadata[key]
+            result = function(*bound.args, **bound.kwargs)
 
-            return args[0]
+            return result
 
+
+    # If we find a manifold parameter in the passed data, we replace its
+    # annotation with napari.layers.Points
+    import inspect
+    from .._stress.manifold_SPB import manifold
+    sig = inspect.signature(wrapper)
+    parameters = []
+    for name, value in sig.parameters.items():
+        print(name, str(value.annotation))
+        if value.annotation is manifold:
+            # replace parameter annotation with napari.layers.Points
+            parameters.append(inspect.Parameter(name, inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation="napari.layers.Points", default=None))
         else:
-            return function(*args, **kwargs)
+            parameters.append(value)
 
-
-        # If a viewer exists...?
+    wrapper.__signature__ = inspect.Signature(parameters, return_annotation=napari.types.LayerDataTuple)
     return wrapper
